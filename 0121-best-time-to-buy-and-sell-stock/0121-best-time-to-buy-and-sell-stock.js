0cd113f9b6aa8f0d29dd01877b0928848f75f73b
Shanoof-t <shanoof.001@gmail.com>
2024-08-25 16:21:10 +0530

    Time: 72 ms (67.26%), Space: 58.9 MB (71.3%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let left=0
+    let right=1
+    let profit=0
+    // while(right<prices.length){
+    //     if(prices[right]>prices[left]){
+    //         if(profit <  prices[right] - prices[left]){
+    //             profit = prices[right] - prices[left]
+    //         }else{
+    //             left = right
+    //         }
+    //     }
+    //     right++
+    // }
+    while(right<prices.length){
+        if(prices[right]>prices[left]){
+            profit = Math.max(profit,prices[right]-prices[left])
+        }else{
+            left = right
+        }
+        right++
+    }
+    return profit
+};
