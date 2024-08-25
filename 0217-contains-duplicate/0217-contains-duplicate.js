245ff6612d532214e0408591b8feda71ffae98ab
Shanoof-t <shanoof.001@gmail.com>
2024-08-25 16:57:18 +0530

    Time: 70 ms (88.8%), Space: 60.3 MB (85.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,15 +3,26 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let left = 0
-    let right =1
-    let sortedNums =nums.sort((a,b)=>a-b)
-    while(right<sortedNums.length){
-        if(sortedNums[right] === sortedNums[left]){
+    // let left = 0
+    // let right =1
+    // let sortedNums =nums.sort((a,b)=>a-b)
+    // while(right<sortedNums.length){
+    //     if(sortedNums[right] === sortedNums[left]){
+    //         return true
+    //     }else{
+    //         left++
+    //         right++
+    //     }
+    // }
+    // return false
+
+    let set =new Set()
+
+    for(x of nums){
+        if(set.has(x)){
             return true
         }else{
-            left++
-            right++
+            set.add(x)
         }
     }
     return false
