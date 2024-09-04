0975d6b8d0e65add976face8afe3c0c25415a4bb
Shanoof-t <shanoof.001@gmail.com>
2024-09-04 18:57:05 +0530

    Time: 73 ms (34.4%), Space: 52.7 MB (10.65%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,15 +3,25 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    let arr=[]
-    for(i=0;i<nums.length;i++){
-        let count=0
-        for(j=0;j<nums.length;j++){
-            if(nums[i] !== nums[j] && nums[i]>nums[j]){
-                count++
-            }
+    // let arr=[]
+    // for(i=0;i<nums.length;i++){
+    //     let count=0
+    //     for(j=0;j<nums.length;j++){
+    //         if(nums[i] !== nums[j] && nums[i]>nums[j]){
+    //             count++
+    //         }
+    //     }
+    //     arr.push(count)
+    // }
+    // return arr
+
+    let newNums = [...nums].sort((a,b)=>a-b)
+    let map = new Map()
+
+    for(i=0;i<newNums.length;i++){
+        if(!map.has(newNums[i])){
+            map.set(newNums[i],i)
         }
-        arr.push(count)
     }
-    return arr
+    return nums.map((num)=>map.get(num))
 };
