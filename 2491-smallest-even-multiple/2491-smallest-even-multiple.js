e1ba744aadc1eaaf75956b58ec774bc0031280a7
Shanoof-t <shanoof.001@gmail.com>
2024-09-23 09:47:33 +0530

    Time: 57 ms (23.98%), Space: 49 MB (27.86%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.js
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -2,17 +2,6 @@
  * @param {number} n
  * @return {number}
  */
-var smallestEvenMultiple = function(n) {
-    let last = Infinity
-    let res = 0
-    for(i=1;i<=last;i++){
-        let mul = n*i
-        if(mul%2==0){
-            res=mul
-            break
-        }else{
-            mul = n*i
-        }
-    }
-    return res
+var smallestEvenMultiple = function (n) {
+    return n % 2 === 0 ? n : n * 2
 };
