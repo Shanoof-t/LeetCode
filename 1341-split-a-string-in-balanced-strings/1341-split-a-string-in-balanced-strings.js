b13fbab8a602529e7c5541d67cd10675ffc31de1
Shanoof-t <shanoof.001@gmail.com>
2024-09-28 16:52:51 +0530

    Time: 47 ms (85.68%), Space: 49.4 MB (23.24%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let lCount =0 
+    let count=0
     let rCount=0
-    let maxCount=0
-    for(let char of s){
-        char === 'R' ? rCount++ : lCount++
-
-        if(rCount === lCount)maxCount++
+    let lCount=0
+    for(let x of s){
+        if(x === 'R'){
+            rCount++
+        }else{
+            lCount++
+        }
+        if(rCount === lCount){
+            count++
+        }
     }
-    return maxCount
+    return count
 };
