66586260db624fb2146b2d01f418391940786377
Shanoof-t <shanoof.001@gmail.com>
2024-09-30 12:57:44 +0530

    Time: 60 ms (12.52%), Space: 48.9 MB (33.57%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.js
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.js
@@ -3,21 +3,33 @@
  * @return {character}
  */
 var repeatedCharacter = function(str) {
-    let s = str.split("")
-    let map={}
-    for(x of s){
-        if(map[x] === undefined){
-            map[x]=1
-        }else{
-            map[x]++
-        }
-        if(map[x] === 2){
+    // let s = str.split("")
+    // let map={}
+    // for(x of s){
+    //     if(map[x] === undefined){
+    //         map[x]=1
+    //     }else{
+    //         map[x]++
+    //     }
+    //     if(map[x] === 2){
+    //         break;
+    //     }
+    // }
+    // for(y in map){
+    //     if(map[y] === 2){
+    //         return y
+    //     }
+    // }
+
+    let res=''
+    let arr=[]
+    for(let x of str){
+        if(!arr.includes(x)){
+            arr.push(x)
+        }else {
+            res = x
             break;
         }
     }
-    for(y in map){
-        if(map[y] === 2){
-            return y
-        }
-    }
+    return res
 };
