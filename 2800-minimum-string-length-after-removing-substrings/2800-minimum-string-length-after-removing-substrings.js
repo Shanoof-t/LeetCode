dd093efbb56d0f52189f6eec56216b121d2941b3
Shanoof-t <shanoof.001@gmail.com>
2024-10-07 13:04:52 +0530

    Time: 88 ms (58.33%), Space: 54.4 MB (21.88%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.js
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.js
@@ -3,13 +3,25 @@
  * @return {number}
  */
 var minLength = function (s) {
-    let stack = []
-    for (let c of s) {
-        if (stack.length > 0 && (stack[stack.length - 1] === 'A' && c === "B") || (stack[stack.length - 1] === 'C' && c === "D")) {
-            stack.pop()
-        } else {
-            stack.push(c)
+    // let stack = []
+    // for (let c of s) {
+    //     if (stack.length > 0 && (stack[stack.length - 1] === 'A' && c === "B") || (stack[stack.length - 1] === 'C' && c === "D")) {
+    //         stack.pop()
+    //     } else {
+    //         stack.push(c)
+    //     }
+    // }
+    // return stack.length
+
+    for (i = 0; i < s.length; i++) {
+        while(s.includes('AB') || s.includes('CD')) {
+            if (s.includes('AB')) {
+                s = s.replace('AB', '')
+            }
+            if (s.includes('CD')) {
+                s = s.replace('CD', '')
+            }
         }
     }
-    return stack.length
+    return s.length
 };
