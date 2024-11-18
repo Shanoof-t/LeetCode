9232c6ff36babb847cac61d6ef71c6a344613838
shanoof <shanoof.001@gmail.com>
2024-11-18 23:52:47 +0530

    Time: 6 ms (97.78%), Space: 57 MB (24.15%) - LeetHub

---
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function (nums1, nums2) {
+    let ans1 = new Set(nums1)
+    nums2.forEach(v => { ans1.delete(v) });
+    let ans2 = new Set(nums2);
+    nums1.forEach(v => { ans2.delete(v) });
+
+    return [[...ans1], [...ans2]];
+};
