838f9bb31915bdb9d9fc0bc13cbd7955f451fbe8
shanoof <shanoof.001@gmail.com>
2024-12-14 11:31:37 +0530

    Time: 6 ms (23.84%), Space: 51.3 MB (38.63%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -1,25 +1,11 @@
-var maxProduct = function(nums) {
-    if (nums.length == 1) {
-        return nums[0];
+var maxProduct = function (nums) {
+    let res = nums[0], curMax = 1, curMin = 1;
+    for (let x of nums) {
+        let temp1 = curMax * x
+        let temp2 = curMin * x
+        curMax = Math.max(temp1, temp2, x)
+        curMin = Math.min(temp1, temp2, x)
+        res = Math.max(curMax,res)
     }
-    // 1 is a number "neutral" to multiplication
-    let firstNegative = 1; // No negatives yet
-    let curPrefix = 1; // Empty yet
-    let maxProduct = 0; // In case we found 0
-    
-    for (let i = 0; i < nums.length; i++) {
-        curPrefix *= nums[i];
-        maxProduct = Math.max(curPrefix, maxProduct);
-        
-        if (curPrefix < 0) { // divide to the first negative
-            maxProduct = Math.max(curPrefix / firstNegative, maxProduct);
-            // set the first negative product, if it wasn't set yet
-            firstNegative = firstNegative == 1? curPrefix : firstNegative;
-            
-        } else if (curPrefix == 0) { // start all over again
-            curPrefix = 1;
-            firstNegative = 1;
-        }
-    }
-    return maxProduct;
+    return res  
 };
