d63b582249f59aab25619e5f6983bf696c77813a
shanoof <shanoof.001@gmail.com>
2025-03-17 09:40:35 +0530

    Time: 3 ms (82.95%), Space: 56.7 MB (16.13%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
@@ -5,7 +5,6 @@
 var divideArray = function (nums) {
     const len = nums.length
     if (len % 2 !== 0) return false
-    let flag = true
     let obj = {}
 
     for (let x of nums) {
@@ -15,11 +14,10 @@ var divideArray = function (nums) {
             obj[x]++
         }
     }
-    console.log(obj)
     for (let y in obj) {
-        if (obj[y] < 2 || obj[y] % 2 !== 0) {
-            flag = false
+        if (obj[y] % 2 !== 0) {
+            return false
         }
     }
-    return flag
+    return true
 };
